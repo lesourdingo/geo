fcb98d21c1f1c0dcf40962f8ff6bdbd420f8f816
Kevin Rumeur <lesourdingo@gmail.com>
2021-03-15 03:47:26 +0100

    add icons

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ fetch('./country.json')
     countries.innerHTML = data.map(country => `
     <div id="Card">
         <div id="flag"><img src="${country.flag}" alt="${country.name}"></div>
-        <div id="name">${country.nameFR}</div>
-        <div id="capital">${country.capitalFR}</div>
+        <div id="name"><i class="fas fa-globe-africa"></i>${country.nameFR}</div>
+        <div id="capital"><i class="fas fa-city"></i>${country.capitalFR}</div>
     </div>
     `).join('');
 });
