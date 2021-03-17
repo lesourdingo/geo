3caf02b252e7deb8ee3946c2d9efdc19ba953a4c
Kevin Rumeur <lesourdingo@gmail.com>
2021-03-17 22:47:03 +0100

    start implementing search option

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
+const main = document.getElementById("main");
+const submit = document.getElementById("submit");
+const search = document.getElementById("search");
 
-
-const countries = document.getElementById('countries');
-
-
-fetch('./country.json')
-.then(response => response.json())
-.then(data => {
-    countries.innerHTML = data.map(country => `
-    <div id="Card">
-        <div id="flag"><img src="${country.flag}" alt="${country.name}"></div>
-        <div id="name"><i class="fas fa-globe-africa"></i>${country.nameFR}</div>
-        <div id="capital"><i class="fas fa-city"></i>${country.capitalFR}</div>
+// Mount country
+fetch("./country.json")
+    .then((response) => response.json())
+    .then((data) => {
+        main.innerHTML = data
+            .map(
+                (country) => `
+    <div class="card">
+        <div class="flag"><img src="${country.flag}" alt="${country.name}"></div>
+        <div class="card-info">
+            <div class="name"><i class="fas fa-globe-africa"></i>${country.nameFR}</div>
+            <div class="capital"><i class="fas fa-city"></i>${country.capitalFR}</div>
+        </div>
     </div>
-    `).join('');
-});
+    `
+            )
+            .join("");
+    });
 
+// Search
+// function searchCountry(e) {
+//     e.preventDefault();
 
+// Get search term
+//     const term = search.value;
 
+//     if (term.trim()) {
+
+//     } else {
+//         alert("Entrer un pays");
+//     }
+// }
+
+// submit.addEventListener("submit", searchCountry);
